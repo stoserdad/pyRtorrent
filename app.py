a18b39fbf2d903cec22b4f252f32bfbb392be0ad
Storozhuk Sergey <storozhuk@omnicomm.ru>
2015-11-06 17:57:05 +0300

    запилил морду от ruTorrent.

--- app.py
+++ app.py
@@ -16,15 +16,17 @@ class Application(tornado.web.Application):
 	def __init__(self):
 		handlers = [
 			(r'/', MainHandler),
-			(r'/static/(.*)', tornado.web.StaticFileHandler,
-			{'path': 'static/'})
+			(r'/css/(.*)', tornado.web.StaticFileHandler, {'path': 'css/'}),
+			(r'/images/(.*)', tornado.web.StaticFileHandler, {'path': 'images/'}),
+			(r'/js/(.*)', tornado.web.StaticFileHandler, {'path': 'js/'}),
+			(r'/lang/(.*)', tornado.web.StaticFileHandler, {'path': 'lang/'})
 		]
 		settings = dict(
 			cookie_secret=''.join(random.choice(string.ascii_uppercase + string.digits + string.ascii_lowercase) for _ in range(35)),
 			xsrf_cookies=True,
 			debug=True,
-			template_path=os.path.join(os.path.dirname(__file__), 'templates'),
-			static_path=os.path.join(os.path.dirname(__file__), 'static')
+			template_path=os.path.join(os.path.dirname(__file__), './'),
+			static_path=os.path.join(os.path.dirname(__file__), './')
 		)
 		tornado.web.Application.__init__(self, handlers, **settings)
 
